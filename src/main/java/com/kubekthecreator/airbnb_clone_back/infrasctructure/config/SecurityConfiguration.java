12ed788e62cc6ffb502b3de022d3b9bac13d16bb
kubekthecreator <50717269+kubekthecreator@users.noreply.github.com>
2024-07-15 19:21:12 +0200

    fix for deployment

--- src/main/java/com/kubekthecreator/airbnb_clone_back/infrasctructure/config/SecurityConfiguration.java
+++ src/main/java/com/kubekthecreator/airbnb_clone_back/infrasctructure/config/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package com.kubekthecreator.airbnb_clone_back.infrasctructure.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -12,7 +13,11 @@ import org.springframework.security.oauth2.core.oidc.user.OidcUserAuthority;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -20,11 +25,16 @@ import java.util.Set;
 @EnableMethodSecurity
 public class SecurityConfiguration {
 
+    @Value("${FRONTEND_URL}")
+    private String frontendUrl;
+
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
         CsrfTokenRequestAttributeHandler requestHandler = new CsrfTokenRequestAttributeHandler();
         requestHandler.setCsrfRequestAttributeName(null);
-        http.authorizeHttpRequests(authorize -> authorize
+        http
+                .cors(Customizer.withDefaults())
+                .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.GET, "api/tenant-listing/get-all-by-category").permitAll()
                         .requestMatchers(HttpMethod.GET, "api/tenant-listing/get-one").permitAll()
                         .requestMatchers(HttpMethod.POST, "api/tenant-listing/search").permitAll()
@@ -39,6 +49,19 @@ public class SecurityConfiguration {
                 .oauth2Client(Customizer.withDefaults());
 
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200", frontendUrl));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
     @Bean
